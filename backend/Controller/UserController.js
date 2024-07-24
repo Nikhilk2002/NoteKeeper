ea858d14ca927f624b30eebcbbe8b4db4e7f50af
Nikhilk2002 <nikhilmkd1523@gmail.com>
2024-07-24 19:55:14 +0530

    Get all notes

--- backend/Controller/UserController.js
+++ backend/Controller/UserController.js
@@ -4,25 +4,33 @@ const Note = require('../model/contentmodel');
 module.exports.addNotes = async (req, res, next) => {
   const { title, content } = req.body;
 
-  // Input validation
   if (!title || !content) {
     return res.status(400).json({ message: 'Title and content are required.' });
   }
 
   try {
-    // Create a new note
     const newNote = new Note({
       title,
       content,
     });
 
-    // Save the note to the database
     await newNote.save();
 
-    // Send a success response
     res.status(201).json({ message: 'Note added successfully', note: newNote });
   } catch (error) {
-    // Handle any errors
+    res.status(500).json({ message: 'Internal Server Error', error: error.message });
+    next(error);
+  }
+};
+
+
+
+module.exports.getAllNotes = async (req, res, next) => {
+  try {
+    const notes = await Note.find();
+
+    res.status(200).json({ message: 'Notes fetched successfully', notes });
+  } catch (error) {
     res.status(500).json({ message: 'Internal Server Error', error: error.message });
     next(error);
   }
--- backend/Routes/UserRouter.js
+++ backend/Routes/UserRouter.js
@@ -1,9 +1,10 @@
 const express= require("express");
-const { addNotes } = require("../Controller/UserController");
+const { addNotes, getAllNotes } = require("../Controller/UserController");
 const router = express.Router();
 
 
 router.post('/addnotes',addNotes)
 
+router.get('/allnotes',getAllNotes)
 
 module.exports = router;
